e96ed185199b6641b010fb68ea0241b6abad8bdb
joaocarloslima <joaocarloslima@me.com>
2024-08-05 09:40:14 -0300

    login tabs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,40 @@
 import { Button } from "@/components/ui/button";
 import { ModeToggle } from "@/components/ui/mode-toggle";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Sphere</h1>
-      <ModeToggle />
-      <Button>Login</Button>
+    <main className="flex min-h-screen items-center justify-between ">
+      <nav className="absolute right-1 top-1">
+        <ModeToggle />
+      </nav>
+
+      <aside className="h-screen hidden lg:block ">
+        <Image src="/login.png" alt="login" width={554} height={832}/>
+      </aside>
+
+      <div>
+        <div className="size-[75px] bg-rose-600 rounded-full"></div>
+        <h1 className="text-4xl font-extrabold uppercase">Sphere</h1>
+        
+        <Tabs defaultValue="entrar" className="w-[400px]">
+          <TabsList className="grid w-full grid-cols-2">
+            <TabsTrigger value="entrar">entrar</TabsTrigger>
+            <TabsTrigger value="registrar">registrar</TabsTrigger>
+          </TabsList>
+          
+          <TabsContent value="entrar">
+            <h1>form de login</h1>
+          </TabsContent>
+
+          <TabsContent value="registrar">
+            <h1>form de conta</h1>
+          </TabsContent>
+        </Tabs>
+
+      </div>
+      
     </main>
   );
 }
